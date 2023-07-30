64aac8d7bb28aae7195d42576ef54cb20654e593
ArgyPorgy <carghya10@gmail.com>
2023-07-31 02:22:39 +0530

    made some changes

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import openai
 import textbase
 from textbase.message import Message
 from textbase import models
@@ -5,7 +6,7 @@ import os
 from typing import List
 
 # Load your OpenAI API key
-models.OpenAI.api_key = "YOUR_API_KEY"
+models.OpenAI.api_key = "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"
 # or from environment variable:
 # models.OpenAI.api_key = os.getenv("OPENAI_API_KEY")
 
@@ -16,23 +17,33 @@ SYSTEM_PROMPT = """You are chatting with an AI. There are no specific prefixes f
 
 @textbase.chatbot("talking-bot")
 def on_message(message_history: List[Message], state: dict = None):
-    """Your chatbot logic here
-    message_history: List of user messages
-    state: A dictionary to store any stateful information
 
-    Return a string with the bot_response or a tuple of (bot_response: str, new_state: dict)
-    """
+    user_message = message_history[-1].content.lower()
+    if "mental health" in user_message:
+        bot_response = "Mental health is an important topic. It refers to the emotional, psychological, and social well-being of an individual. Taking care of your mental health is essential for overall well-being and quality of life. If you have specific questions or concerns about mental health, feel free to share, and I'll do my best to provide helpful information."
+    if "down" in user_message:
+        bot_response = "I'm really sorry to hear that you're feeling this way. It's important to talk about these feelings. Can you tell me more about what's been bothering you lately?"
+        
+    if "stress" in user_message:
+        bot_response  = "Stress and anxiety can be challenging to cope with. Remember to take breaks, practice deep breathing, and consider talking to someone you trust or a mental health professional for support."
+            
+    if "anxiety" in user_message:
+        bot_response = "Stress and anxiety can be challenging to cope with. Remember to take breaks, practice deep breathing, and consider talking to someone you trust or a mental health professional for support."
 
-    if state is None or "counter" not in state:
-        state = {"counter": 0}
+    if "difficult decision " in user_message:
+      bot_response = "Making decisions can be tough. Take some time to weigh the pros and cons, and consider talking it through with someone you trust to gain perspective."
+
+    if "confidence" in user_message:
+        bot_response = "Building self-esteem takes time and self-compassion. Focus on your strengths, set achievable goals, and celebrate your accomplishments, no matter how small."
+
+
     else:
-        state["counter"] += 1
 
-    # # Generate GPT-3.5 Turbo response
-    bot_response = models.OpenAI.generate(
+        bot_response = models.OpenAI.generate(
         system_prompt=SYSTEM_PROMPT,
         message_history=message_history,
         model="gpt-3.5-turbo",
+       
     )
 
     return bot_response, state
